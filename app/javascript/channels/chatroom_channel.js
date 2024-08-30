748920c8a93b57a8a894749b416d3fea7361b1ae
Razvan Cojei <razvanytb1@gmail.com>
2024-08-30 15:31:30 +0300

    Generate chatroom channel and add route

---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,16 @@
+import consumer from "channels/consumer"
+
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    alert(data.foo);
+    console.log("Received message:", data.foo);
+  }
+});
